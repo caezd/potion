2b41e5fdfe74435cff71c64c126c5e5d018c912b
Monomer <caedrikbl@gmail.com>
2025-01-28 11:57:39 -0500

    ignore

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+    input: "src/main.js",
+    output: [
+        {
+            file: "dist/potion.js",
+            format: "iife",
+        },
+    ],
+    plugins: [
+        commonjs(),
+        resolve({
+            browser: true,
+        }),
+    ],
+};
+
+/* import { readFileSync } from "fs";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import terser from "@rollup/plugin-terser";
+
+const packageJson = JSON.parse(readFileSync("./package.json"));
+
+export default {
+    input: "./src/main.js",
+    output: [
+        {
+            name: "poumon",
+            file: packageJson.browser,
+            format: "umd",
+        },
+        {
+            file: packageJson.module,
+            format: "es",
+        },
+    ],
+    plugins: [
+        terser(),
+        external({ includeDependencies: true }),
+        resolve(),
+        commonjs(),
+    ],
+};
+ */
